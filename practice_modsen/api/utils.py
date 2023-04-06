4306f6fce3d8b8d644974978b43cbce4ccb1cbde
DenisKitarov <81997155+DenisKitarov@users.noreply.github.com>
2023-04-06 23:09:21 +0300

    Add files via upload

--- practice_modsen/api/utils.py
+++ practice_modsen/api/utils.py
@@ -1,41 +1,24 @@
+import os
+import logging
 from elasticsearch import Elasticsearch
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import SQLAlchemyError
-from .models import Document
-from .config import ELASTICSEARCH_HOST, ELASTICSEARCH_PORT, SQLALCHEMY_DATABASE_URI
+from elasticsearch.helpers import bulk
 
-# Создаем объекты для работы с базой данных и Elasticsearch
-db = SQLAlchemy()
-es = Elasticsearch([{'host': ELASTICSEARCH_HOST, 'port': ELASTICSEARCH_PORT}])
+logger = logging.getLogger(__name__)
+es = Elasticsearch([os.environ.get('ELASTICSEARCH_URL', 'http://localhost:9200')])
 
-def search_documents(query):
-    # Поиск документов в Elasticsearch
-    es_query = {
-        'query': {
-            'multi_match': {
-                'query': query,
-                'fields': ['text']
+def index_documents(documents):
+    """Index a list of documents in Elasticsearch"""
+    actions = []
+    for doc in documents:
+        action = {
+            '_index': 'documents',
+            '_id': doc['id'],
+            '_source': {
+                'title': doc['title'],
+                'content': doc['content'],
+                'timestamp': doc['timestamp']
             }
-        },
-        'sort': [{'created_date': 'desc'}],
-        'size': 20
-    }
-    search_result = es.search(index='documents', body=es_query)
-    hits = search_result['hits']['hits']
-
-    # Получение документов из базы данных по id из Elasticsearch
-    document_ids = [hit['_id'] for hit in hits]
-    documents = Document.query.filter(Document.id.in_(document_ids)).all()
-
-    # Формируем результат в виде списка словарей
-    result = []
-    for hit, document in zip(hits, documents):
-        result.append({
-            'id': document.id,
-            'rubrics': document.rubrics,
-            'text': document.text,
-            'created_date': document.created_date,
-            'score': hit['_score']
-        })
-
-    return result
+        }
+        actions.append(action)
+    bulk(es, actions)
+    logger.info(f"{len(actions)} documents indexed")
